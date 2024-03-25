f351453a54e989388f275e85eafa14c8ebe83eca
Almish Subuktegin <almishsubuk@gmail.com>
2024-03-24 17:09:40 -0700

    stuff

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="about me" />
+      </div>
+      <div className="hero--section--content--boxabout--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+            ipsum laborum nobis repellat incidunt asperiores dolorem dolor neque
+            minus blanditiis. Suscipit repudiandae quo at excepturi eaque hic
+            impedit repellendus deleniti.
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+            ipsum laborum nobis repellat incidunt asperiores dolorem dolor neque
+            minus blanditiis.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,27 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Almish</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full Stack</span>{" "}
+            <br />
+            <span className="hero--section--title--color">Developer</span>
+          </h1>
+          <p className="hero--section--description">
+            I'm a 13 year old developer who deals with full stack applications.
+            <br />I love programming and can work with a variety of languages
+            and frameworks.
+          </p>
+        </div>
+        <a href="#MyPortfolio" className="btn btn-primary">
+          See What I've Done
+        </a>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="hero section" />
+      </div>
+    </section>
+  );
+}
