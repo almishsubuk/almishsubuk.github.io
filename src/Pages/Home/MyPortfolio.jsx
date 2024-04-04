024de271b5954d86c075445328e5664a78f1c596
Almish Subuktegin <almishsubuk@gmail.com>
2024-04-03 19:38:22 -0700

    added a wrap to the projects so there can be a bunch

--- src/Pages/Home/MyPortfolio.jsx
+++ src/Pages/Home/MyPortfolio.jsx
@@ -1,19 +1,14 @@
+import React from "react";
+import Slider from "react-slick";
 import data from "../../data/index.json";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function MyPortfolio() {
   return (
     <section className="portfolio--section" id="MyPortfolio">
-      <div className="portfolio--container-box">
-        <div className="portfolio--container">
-          <p className="sub--title">Recent Projects</p>
-          <h2 className="section--heading">My Portfolio</h2>
-        </div>
-        <div>
-          <button className="btn btn-github">
-            <a href="https://github.com/yalikejazz1337">Visit My GitHub</a>
-          </button>
-        </div>
-      </div>
+      {/* Your existing portfolio header content */}
+
       <div className="portfolio--section--container">
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio--section--card">
@@ -38,9 +33,9 @@ export default function MyPortfolio() {
                     <path
                       d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
                       stroke="currentColor"
-                      stroke-width="2.66667"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2.66667"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </svg>
                 </p>
